4d256a9839aea32cd502caf2b71381bdda622623
DanielArguirov <69607099+DanielArguirov@users.noreply.github.com>
2022-05-15 10:41:20 -0400

    Changed dc15x and dp23

--- epsilon_tfa_swheat/lua/weapons/epsilon_dp23.lua
+++ epsilon_tfa_swheat/lua/weapons/epsilon_dp23.lua
@@ -151,8 +151,8 @@ SWEP.ViewModelBoneMods = {
 
 SWEP.VElements = {
 	["dp23"] = { type = "Model", model = "models/cs574/weapons/dp23.mdl", bone = "v_dlt19_reference001", rel = "", pos = Vector(0.75, 0.5, 0.5), angle = Angle(0, -90, 0), size = Vector(1.2, 1.1, 1.2), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {[1] = 0, [2] = 1, [3] = 1} },
-    ["case"] = { type = "Model", model = "models/dolunity/heatometer/heatometer.mdl", bone = "v_dlt19_reference001", rel = "dp23", pos = SWEP.heatDisplayPos, angle = SWEP.heatDisplayRot, size = Vector(SWEP.heatDisplayX,SWEP.heatDisplayY, SWEP.heatDisplayZ), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
-	["heat"] = { type = "Model", model = "models/hunter/plates/plate025.mdl", bone = "v_dlt19_reference001", rel = "case", pos = Vector(0, 0, 0.02), angle = Angle(0, 0, 0), size = Vector(SWEP.heatDisplayX * 0.6, SWEP.heatDisplayY * 0.7333, SWEP.heatDisplayZ * 0.1), color = Color(255, 255, 255, 255), surpresslightning = true, material = "models/debug/debugwhite", skin = 0, bodygroup = {} },
+    ["case"] = { type = "Model", model = "models/dolunity/heatometer/heatometer.mdl", bone = "", rel = "dp23", pos = SWEP.heatDisplayPos, angle = SWEP.heatDisplayRot, size = Vector(SWEP.heatDisplayX,SWEP.heatDisplayY, SWEP.heatDisplayZ), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["heat"] = { type = "Model", model = "models/hunter/plates/plate025.mdl", bone = "", rel = "case", pos = Vector(0, 0, 0), angle = Angle(0, 0, 0), size = Vector(SWEP.heatDisplayX * 0.6, SWEP.heatDisplayY * 0.7333, SWEP.heatDisplayZ * 0.1), color = Color(255, 255, 255, 255), surpresslightning = true, material = "models/debug/debugwhite", skin = 0, bodygroup = {} },
 }	
 
 SWEP.WElements = {
